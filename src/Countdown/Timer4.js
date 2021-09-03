c87a80c37399c4d9062f8e86d9731cf2129a0c9d
beep-boop-stof <loveleenbassi761@gmail.com>
2021-09-02 20:12:31 -0700

    fixed TypeError issue on the timers page

--- src/Countdown/Timer4.js
+++ src/Countdown/Timer4.js
@@ -16,8 +16,10 @@ const Timer4 = () => {
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 	// Display the result in the element with id="demo"
-	document.getElementById("demo4").innerHTML = days + "d " + hours + "h "
-	+ minutes + "m " + seconds + "s ";
+	window.onload = function () {
+		document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+		+ minutes + "m " + seconds + "s ";
+	}
 	// If the count down is finished, write some text
 	if (distance < 0) {
 		clearInterval(x);
